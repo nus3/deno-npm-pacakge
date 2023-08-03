6771dad34ab6ae52a4c282a154eaa693902cdd60
nus3 <hadayota33@gmail.com>
2023-08-03 11:05:44 +0900

    add ci action

--- mod/index_test.ts
+++ mod/index_test.ts
@@ -1,5 +1,5 @@
 import { assertStringIncludes } from "https://deno.land/std@0.192.0/testing/asserts.ts";
-import { cowsayNus3 } from "./mod.ts";
+import { cowsayNus3 } from "./index.ts";
 
 Deno.test("Cow should say Hello nus3!", () => {
   assertStringIncludes(cowsayNus3(), "Hello nus3!");
