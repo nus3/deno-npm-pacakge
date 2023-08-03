36c456d7641a9111e419d67023207d0472408627
nus3 <hadayota33@gmail.com>
2023-08-03 10:34:51 +0900

    add example

---
+++ example/cjs.ts
@@ -0,0 +1,7 @@
+const mod = require("@nus3/cowsay-nus3");
+
+const main = () => {
+  console.log(mod.cowsayNus3());
+};
+
+main();
---
+++ example/esm.ts
@@ -0,0 +1,7 @@
+import { cowsayNus3 } from "@nus3/cowsay-nus3";
+
+const main = () => {
+  console.log(cowsayNus3());
+};
+
+main();
